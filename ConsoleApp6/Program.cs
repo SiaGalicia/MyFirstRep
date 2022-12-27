2640121b16496a583b017b234683e8941460600b
Nastya <Nastya@DESKTOP-BUKO0IR>
2022-12-27 16:10:36 +0100

    Agregar archivos de proyecto.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+public class Ejemplo_05_11a 
+{ 
+    public static int Main(string[] args) 
+    {
+        int x, y;
+        try
+        {
+            x = Convert.ToInt32(args[0]);
+        }
+        catch (FormatException)
+        {
+            Environment.Exit(2);
+        }
+        try
+        {
+            y = Convert.ToInt32(args[0]);
+        }
+        catch (FormatException)
+        {
+            Environment.Exit(2);
+        }
+
+        switch (args[1])
+            {
+                case "+":
+                    Console.WriteLine(Convert.ToInt32(args[0]) + (Convert.ToInt32(args[2]))); return 0;
+                case "-":
+                    Console.WriteLine(Convert.ToInt32(args[0]) - (Convert.ToInt32(args[2]))); return 0;
+                case "*":
+                    Console.WriteLine(Convert.ToInt32(args[0]) * (Convert.ToInt32(args[2]))); return 0;
+                case "/":
+                    Console.WriteLine(Convert.ToInt32(args[0]) / (Convert.ToInt32(args[2]))); return 0;
+                default: Environment.Exit(1); break;
+            }
+
+          return 0;
+    } 
+}
